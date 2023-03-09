cfb163f88aad69daacacac67da58f074612bf493
Emily Zhai <ezhai24@gmail.com>
2023-03-08 20:29:47 -0500

    remove unused font face definition

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,10 +13,6 @@ export default function Document() {
               src: url('/fonts/mine-sweeper.ttf');
             }
             @font-face {
-              font-family: 'Futuba';
-              src: url('/fonts/futaba-digital-7.ttf');
-            }
-            @font-face {
               font-family: 'LCD Calculator';
               src: url('/fonts/lcd-calculator-display-tight-7-segment.ttf');
             }

6fe4e627f047d362d5df93615bf6604113b9a3be
Emily Zhai <ezhai24@gmail.com>
2023-10-16 22:03:22 -0400

    center content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head';
 
 import type { AppProps } from 'next/app';
 
+import './globals.css';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>

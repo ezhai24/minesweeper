ad2e2aec06b19326506441626e5578e1a361377f
Emily Zhai <ezhai24@gmail.com>
2023-10-16 22:58:42 -0400

    add an h1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
+import styled from '@emotion/styled';
+
 import { Credits } from '@/components/Credits';
 import Minesweeper from '@/components/Minesweeper';
 
+const PageTitle = styled.h1({
+  marginTop: 35,
+});
+
 const Home = () => (
   <>
+    <PageTitle>Minesweeper</PageTitle>
     <Minesweeper />
     <Credits />
   </>
